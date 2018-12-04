3b622ef070e670ca982b8b7eebb985f69e1ecfe3
Senan <senan_996@hotmail.com>
2018-12-04 13:56:54 +0100

    He añadido pruebas para probar cosas ahi

---
+++ src/main/scala/Pruebas/test.scala
@@ -0,0 +1,11 @@
+package Pruebas
+
+import scalafx.application.JFXApp
+
+object Prueba extends JFXApp{
+  stage = new JFXApp.PrimaryStage {
+    title = "First GUI"
+    width = 500
+    height = 500
+  }
+}
